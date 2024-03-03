015efb36006d6466a98f76e5b0511686e814169f
Luiz Furmann <furmannluiz@gmail.com>
2024-03-03 16:44:49 -0300

    criado adatpter

--- app/src/main/java/com/example/commerceapp/view/MainActivity.java
+++ app/src/main/java/com/example/commerceapp/view/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.commerceapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.commerceapp.R;
+import com.example.commerceapp.view.product.ProductViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ProductViewModel productViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    private void setupViewModel(){
+
+    }
+
+    private void setupRecyclerView(){
+
     }
 }
---
+++ app/src/main/java/com/example/commerceapp/view/product/ProductAdapter.java
@@ -0,0 +1,50 @@
+package com.example.commerceapp.view.product;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.commerceapp.R;
+import com.example.commerceapp.model.Product;
+
+import java.util.ArrayList;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.StudentViewHolder> {
+    ArrayList<Product> productArrayList;
+
+    public ProductAdapter(ArrayList<Product> students) {
+        this.productArrayList = students;
+    }
+
+    @NonNull
+    @Override
+    public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row_product, parent, false);
+        return new StudentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productArrayList.size();
+    }
+
+    static class StudentViewHolder extends RecyclerView.ViewHolder {
+
+        public StudentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            findViews();
+        }
+
+        private void findViews() {
+        }
+    }
+}
